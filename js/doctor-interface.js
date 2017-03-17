0625e5b25284e1793cb300392c2188d535a18724
Clayton Collins <claytonccollins@gmail.com>
2017-03-17 12:19:07 -0700

    working api call

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,3 +1,7 @@
+var Doctor = require('./../js/doctor.js').doctorModule;
+
 $(function() {
-  console.log('hello');
+  var doctore = new Doctor();
+  var medicalIssue = 'toothache';
+  doctore.getDoctors(medicalIssue);
 });
