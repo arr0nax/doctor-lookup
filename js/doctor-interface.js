ff2055dffb05a76e5011ae4e942ecb314c5c9952
Clayton Collins <claytonccollins@gmail.com>
2017-03-17 13:47:05 -0700

    display names of doctors close to you and error message if search is unsuccessful

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,35 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var noresults = function() {
+  $('.displayresults').hide();
+  $('.noresults').fadeIn();
+};
+
+var displayresults = function(doctor) {
+  $('.noresults').hide();
+  $('.displayresults').empty();
+  $('.displayresults').append("<h3>here are some doctors you might be interested in seeing: "+doctor.list+"</h3>");
+  $('.displayresults').fadeIn();
+};
+
 $(function() {
+  var lat = 45.5;
+  var lon = -122.6;
+  if ('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      $('#waiting_message').hide();
+      $('.search').fadeIn();
+    });
+  } else {
+    $('#waiting_message').hide();
+    $('.search').fadeIn();
+  }
   var doctore = new Doctor();
-  var medicalIssue = 'toothache';
-  doctore.getDoctors(medicalIssue);
+  $('.search').submit(function(event){
+    event.preventDefault();
+    var medicalIssue = $('#medicalIssue').val();
+    doctore.getDoctors(medicalIssue,lat,lon,noresults,displayresults);
+  });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,15 +3,28 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue,lat,lon,noresults,displayresults) {
   console.log(medicalIssue);
   console.log(apiKey);
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+  var doctor = this;
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location='+lat+'%2C'+lon+'%2C100&user_location='+lat+'%2C'+lon+'&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(response) {
+      if(response.data.length > 0) {
+        console.log(response);
+        doctor.result = response;
+        doctor.list = [];
+        for(var i = 0; i<response.data.length; i++){
+          for(var j=0; j<response.data[i].practices.length;j++){
+            doctor.list.push(response.data[i].practices[j].name);
+          }
+        }
+        displayresults(doctor);
+      } else {
+        noresults();
+      }
     })
    .fail(function(error){
-      console.log("fail");
+      noresults();
     });
 };
 
